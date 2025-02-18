59f12ee71d04d2ea752d6d63e27bde7a0966a7bb
Flaviojcf <flaviojcostafilho@gmail.com>
2025-02-18 13:43:40 -0300

    feat: Add routing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,37 +1,62 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
+import { Location } from '@angular/common';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ComponentFixture, fakeAsync, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { RestaurantComponent } from './components/restaurant/restaurant.component';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterModule.forRoot([])
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [AppRoutingModule],
+      declarations: [AppComponent, HomeComponent, RestaurantComponent],
       schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'project'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  it(`should have as title 'place-my-order'`, () => {
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('project');
+    expect(app.title).toEqual('place-my-order');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  it('should render title in a h1 tag', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Place My Order App: Coming Soon!');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Place My Order App: Coming Soon!'
+    );
   });
+
+  it('should render the HomeComponent with router navigates to "/" path', fakeAsync(() => {
+    const location: Location = TestBed.inject(Location);
+    const router: Router = TestBed.inject(Router);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    router.navigate(['']).then(() => {
+      expect(location.path()).toBe('');
+      expect(compiled.querySelector('pmo-home')).not.toBe(null);
+    });
+  }));
+
+  it('should render the RestaurantsComponent with router navigates to "/restaurants" path', fakeAsync(() => {
+    const location: Location = TestBed.inject(Location);
+    const router: Router = TestBed.inject(Router);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    router.navigate(['restaurants']).then(() => {
+      expect(location.path()).toBe('/restaurants');
+      expect(compiled.querySelector('pmo-restaurant')).not.toBe(null);
+    });
+  }));
 });
