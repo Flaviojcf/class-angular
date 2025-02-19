084cd3ee1a33482108b2b5f61deab8989c013bed
Flaviojcf <flaviojcostafilho@gmail.com>
2025-02-19 12:58:51 -0300

    feat: Add socket IO

--- src/app/components/order/history/history.component.ts
+++ src/app/components/order/history/history.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subject, takeUntil } from 'rxjs';
+import io from 'socket.io-client';
+import { environment } from '../../../../environments/environment';
 import { Order, OrderService } from '../../../services/order/order.service';
 import { ResponseData } from '../../../services/restaurant/restaurant.service';
-
 
 interface Data<T> {
   value: T[];
@@ -18,14 +19,37 @@ interface Data<T> {
 export class HistoryComponent implements OnInit, OnDestroy {
   orders: Data<Order> = { value: [], isPending: true };
   private onDestroy$ = new Subject<void>();
+  socket: SocketIOClient.Socket;
 
-  constructor(private orderService: OrderService) {}
+  constructor(private orderService: OrderService) {
+    this.socket = io(environment.apiUrl);
+  }
 
   ngOnInit(): void {
     this.getOrders();
+
+    this.socket.on('orders created', (order: Order) => {
+      this.orders.value.push(order);
+    });
+
+    this.socket.on('orders updated', (order: Order) => {
+      const orderIndex = this.orders.value.findIndex(
+        (item) => item._id === order._id
+      );
+      this.orders.value.splice(orderIndex, 1);
+      this.orders.value.push(order);
+    });
+
+    this.socket.on('orders removed', (order: Order) => {
+      const orderIndex = this.orders.value.findIndex(
+        (item) => item._id === order._id
+      );
+      this.orders.value.splice(orderIndex, 1);
+    });
   }
 
   ngOnDestroy(): void {
+    this.socket.removeAllListeners();
     this.onDestroy$.next();
     this.onDestroy$.complete();
   }
