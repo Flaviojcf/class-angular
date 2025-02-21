b8db68465e0d56fa72b42ef78c705fb3f22eb831
Flaviojcf <flaviojcostafilho@gmail.com>
2025-02-21 15:14:30 -0300

    refactor: Change environment

--- src/app/services/restaurant/restaurant.service.ts
+++ src/app/services/restaurant/restaurant.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from '../../../environments/environment.development';
 import { Restaurant } from '../../interfaces/restaurant/restaurant';
+import { environment } from '../../../environments/environment';
 
 
 export interface ResponseData<DataType> {
